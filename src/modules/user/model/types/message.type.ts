c97dee131de477ceea14943b0680764a7db87c5f
Jonath-z <jonathanzihindula95@gmail.com>
2022-05-29 18:19:55 +0200

    add message columns

--- src/modules/user/model/types/message.type.ts
+++ src/modules/user/model/types/message.type.ts
@@ -1,16 +1,30 @@
 import { Field, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryColumn } from 'typeorm';
 
 @ObjectType()
+@Entity('messages')
 export class Message {
+  @PrimaryColumn()
+  @Column()
+  id: number;
+
   @Field()
+  @Column()
   from: string;
 
   @Field()
+  @Column()
   to: string;
 
   @Field()
+  @Column()
   message: string;
 
   @Field()
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  sentAt: Date;
+
+  @Field()
+  @Column()
   messageId: string;
 }
